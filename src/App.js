eb79b2e7fc1adc1b6c4231568ae5996fb827b597
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2022-04-14 15:44:15 +0530

    navbar and stateless component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
+import React, {Component} from 'react';
 import './App.css';
 import Counters from "./components/counters";
+import Navbar from "./components/navbar";
 
-function App() {
-    return (
-        <main className="container mt-2">
-            <Counters/>
-        </main>
-    );
+export default class App extends Component {
+    state = {
+        counters: [
+            {
+                id: 1,
+                value: 1,
+                title: 'Shubham counter'
+            },
+            {
+                id: 2,
+                value: -12,
+                title: 'Harshit counter'
+            },
+            {
+                id: 3,
+                value: 100,
+                title: 'Gautam counter'
+            }
+        ]
+    }
+
+    onDelete = (id) => {
+        const counters = this.state.counters.filter(counter => counter.id !== id);
+        this.setState({
+            counters
+        });
+    }
+
+    onReset = () => {
+        const counters = this.state.counters.map(counter => {
+            counter.value = 0;
+            return counter;
+        });
+        this.setState({
+            counters
+        });
+    }
+
+    onIncrement = (e, id) => {
+        // console.log(e, e.nativeEvent);
+        const counterIndex = this.state.counters.findIndex(counter => counter.id === id);
+        this.state.counters[counterIndex].value++;
+        this.setState({
+            counters: this.state.counters
+        });
+    }
+
+    onDecrement = (e, id) => {
+        // console.log(e, e.nativeEvent);
+        const counterIndex = this.state.counters.findIndex(counter => counter.id === id);
+        this.state.counters[counterIndex].value--;
+        this.setState({
+            counters: this.state.counters
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <main className="container mt-2">
+                    <Counters
+                        counters={this.state.counters}
+                        onDecrement={this.onDecrement}
+                        onIncrement={this.onIncrement}
+                        onReset={this.onReset}
+                        onDelete={this.onDelete}
+                    />
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
-export default App;
+
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+        </nav>
+    )
+}
+
+export default Navbar;
